cdae855207030a82906e23ed3dc38ef06c8284f4
Turkan Isayeva <turkantech@gmail.com>
2026-03-25 02:19:00 +0400

    pinecone index creation

---
+++ src/chatbot/index.ts
@@ -0,0 +1,17 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+
+const pc = new Pinecone({
+  apiKey: process.env.PINECONE_KEY!,
+});
+
+const indexName = "default";
+await pc.createIndexForModel({
+  name: indexName,
+  cloud: "aws",
+  region: "us-east-1",
+  embed: {
+    model: "llama-text-embed-v2",
+    fieldMap: { text: "chunk_text" },
+  },
+  waitUntilReady: true,
+});
