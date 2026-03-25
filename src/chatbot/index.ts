614ae95a3a9424b36a67844c34171147a8603ebd
Turkan Isayeva <turkantech@gmail.com>
2026-03-25 12:29:15 +0400

    create indexes, namespacing, upsert index, query index

--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -5,13 +5,66 @@ const pc = new Pinecone({
 });
 
 const indexName = "default";
-await pc.createIndexForModel({
-  name: indexName,
-  cloud: "aws",
-  region: "us-east-1",
-  embed: {
-    model: "llama-text-embed-v2",
-    fieldMap: { text: "chunk_text" },
-  },
-  waitUntilReady: true,
-});
+type MetaData = {
+  coolness: number;
+  reference: string;
+};
+async function listIndexes() {
+  const result = await pc.listIndexes();
+  console.log(result);
+}
+
+// await pc.createIndexForModel({
+//   name: indexName,
+//   cloud: "aws",
+//   region: "us-east-1",
+//   embed: {
+//     model: "llama-text-embed-v2",
+//     fieldMap: { text: "chunk_text" },
+//   },
+//   waitUntilReady: true,
+// });
+
+// listIndexes()
+
+function getIndex() {
+  return pc.index<MetaData>(indexName).namespace("new-index");
+}
+// getIndex();
+
+function getEmbeddings(length: number) {
+  return Array.from({ length }, () => Math.random());
+}
+
+async function upsertIndexes() {
+  const embed = getEmbeddings(1024);
+  const index = getIndex();
+  await index.upsert({
+    records: [
+      {
+        id: "id-1",
+        values: embed,
+        metadata: {
+          coolness: 3,
+          reference: "hey",
+        },
+      },
+    ],
+  });
+}
+
+// upsertIndexes();
+
+async function queryIndex() {
+  const index = getIndex();
+  const result = await index.query({
+    // query based on either id or vector 
+    id: "id-1",
+    topK: 1,
+    includeMetadata: true
+  });
+  console.log(result);
+  
+}
+
+queryIndex()
