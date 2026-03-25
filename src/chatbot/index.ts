6d050dd0807d9cad17e0f9d8a19db8ab5031af0c
Turkan Isayeva <turkantech@gmail.com>
2026-03-25 19:06:32 +0400

    saved predefined embeddings for chatbot

--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -1,70 +1,70 @@
+import { OpenAI } from "openai";
 import { Pinecone } from "@pinecone-database/pinecone";
 
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 const pc = new Pinecone({
   apiKey: process.env.PINECONE_KEY!,
 });
 
-const indexName = "default";
-type MetaData = {
-  coolness: number;
-  reference: string;
+const studentInfo = `Alexandra Thompson, a 19-year-old computer science sophomore with a 3.7 GPA,
+is a member of the programming and chess clubs who enjoys pizza, swimming, and hiking
+in her free time in hopes of working at a tech company after graduating from the University of Washington.`;
+
+const clubInfo = `The university chess club provides an outlet for students to come together and enjoy playing
+the classic strategy game of chess. Members of all skill levels are welcome, from beginners learning
+the rules to experienced tournament players. The club typically meets a few times per week to play casual games,
+participate in tournaments, analyze famous chess matches, and improve members' skills.`;
+
+const universityInfo = `The University of Washington, founded in 1861 in Seattle, is a public research university
+with over 45,000 students across three campuses in Seattle, Tacoma, and Bothell.
+As the flagship institution of the six public universities in Washington state,
+UW encompasses over 500 buildings and 20 million square feet of space,
+including one of the largest library systems in the world.`;
+
+type Info = {
+  info: string;
+  relevance: number;
+  referance: string;
 };
-async function listIndexes() {
-  const result = await pc.listIndexes();
-  console.log(result);
-}
 
-// await pc.createIndexForModel({
-//   name: indexName,
-//   cloud: "aws",
-//   region: "us-east-1",
-//   embed: {
-//     model: "llama-text-embed-v2",
-//     fieldMap: { text: "chunk_text" },
-//   },
-//   waitUntilReady: true,
-// });
+// convert data into vector with chunks
+const dataToEmbed: Info[] = [
+  {
+    info: studentInfo,
+    relevance: 0.9,
+    referance: "student 123",
+  },
+  {
+    info: clubInfo,
+    relevance: 0.8,
+    referance: "club 456",
+  },
+  {
+    info: universityInfo,
+    relevance: 0.7,
+    referance: "uni 789",
+  },
+];
 
-// listIndexes()
+const pcIndex = pc.index<Info>("search-index");
 
-function getIndex() {
-  return pc.index<MetaData>(indexName).namespace("new-index");
-}
-// getIndex();
+async function storeEmbeddings() {
+  const { data } = await client.embeddings.create({
+    model: "text-embedding-3-small",
+    input: dataToEmbed.map((d) => d.info),
+    dimensions:1024
+  });
 
-function getEmbeddings(length: number) {
-  return Array.from({ length }, () => Math.random());
-}
+  const sorted = [...data].sort((a, b) => a.index - b.index);
 
-async function upsertIndexes() {
-  const embed = getEmbeddings(1024);
-  const index = getIndex();
-  await index.upsert({
-    records: [
-      {
-        id: "id-1",
-        values: embed,
-        metadata: {
-          coolness: 3,
-          reference: "hey",
-        },
-      },
-    ],
+  await pcIndex.upsert({
+    records: sorted.map((row, index) => ({
+      id: `id-${index}`,
+      values: row.embedding,
+      metadata: dataToEmbed[index],
+    })),
   });
 }
-
-// upsertIndexes();
-
-async function queryIndex() {
-  const index = getIndex();
-  const result = await index.query({
-    // query based on either id or vector 
-    id: "id-1",
-    topK: 1,
-    includeMetadata: true
-  });
-  console.log(result);
-  
-}
-
-queryIndex()
+storeEmbeddings();
